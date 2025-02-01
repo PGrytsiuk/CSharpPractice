0d87d3e4a974d5d8cd295643970131a5d86355cc
PGrytsiuk <pgrytsiuk1992@gmail.com>
2025-02-01 22:29:00 +0200

    refactoring

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -18,6 +18,10 @@ class Program
             Console.WriteLine("5. Exit");
             String choice = Console.ReadLine();
             NewMethod(choice);
+            if (choice == "5" )
+            {
+                break;
+            }
         }
     }
 
