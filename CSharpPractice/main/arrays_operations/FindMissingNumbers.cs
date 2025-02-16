40b4ee692367a86190823437daf1a6dc5513838c
PGrytsiuk <pgrytsiuk1992@gmail.com>
2025-02-16 20:02:27 +0200

    addedFindedMissingNumber

---
+++ CSharpPractice/main/arrays_operations/FindMissingNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.main.arrays_operations
+{
+    public class FindMissingNumbers
+    {
+        public static void FindMissingNumberMethod(int[] arr) {
+           int  n = arr.Length + 1;
+            int total = n * (n + 1) / 2;
+            foreach (int num in arr) { 
+                total -= num;
+            }
+            Console.WriteLine("The missing number is: " + total);
+        }
+    }
+}
