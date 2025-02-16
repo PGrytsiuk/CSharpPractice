6af79e2111cb85830cbcd883de059f2605a8003e
Pavlo <33345335+PGrytsiuk@users.noreply.github.com>
2025-02-16 20:57:13 +0200

    Develop (#20)
    * addedNewTasks
    
    * refactoring
    
    * refactoringV1.0
    
    * CountDuplicatedIntegers
    
    * addedFindedMissingNumber
    
    * AddedNewTasks

---
+++ CSharpPractice/main/arrays_operations/FindLargestElemeintInArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.main.arrays_operations
+{
+    public class FindLargestElemeintInArray
+    {
+        public static void FindLargestElementMethod(int[] arr)
+        {
+            int largestElemet = arr[0];
+            foreach (int num in arr)
+            {
+                if (num > largestElemet)
+                {
+                    largestElemet = num;
+                }
+            }
+            Console.WriteLine("Largest element in the array is: " + largestElemet);
+        }
+    }
+}
---
+++ CSharpPractice/main/arrays_operations/MergeTwoArrays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.main.arrays_operations
+{
+    internal class MergeTwoArrays
+    {
+        public static void MergeTwoArraysMethod(int[] arr, int[] arr2)
+        {
+            int[] mergedArray = new int[arr.Length + arr2.Length];
+            arr.CopyTo(mergedArray, 0);
+            arr2.CopyTo(mergedArray, arr.Length);
+            Console.WriteLine("Merged array: " + string.Join(", ", mergedArray));
+        }
+    }
+}
