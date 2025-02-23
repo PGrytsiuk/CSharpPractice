1bb608c9fd5eb5085e7ff708e425a284cb3ce975
PGrytsiuk <pgrytsiuk1992@gmail.com>
2025-02-23 19:56:31 +0200

    AddedRemoveDuplicatesMEthods

---
+++ CSharpPractice/main/arrays_operations/RemoveDuplicatesInArray.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.NetworkInformation;
+
+namespace CSharpPractice.main.arrays_operations
+{
+    public class RemoveDuplicatesInArray
+    {
+        public void RemoveDuplicatesInArrayMethod(int[] arr)
+        {
+            HashSet<int> hashSet = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++)
+            { 
+                hashSet.Add(arr[i]);
+            }
+            Console.WriteLine("Array after removing duplicates: " + string.Join(", ", hashSet));
+        }
+
+        public void RemoveDuplicatesWithStreams(int[] arr)
+        {
+            int[] uniqueArray = arr.Distinct().ToArray();
+            Console.WriteLine("Array after removing duplicates: " + string.Join(", ", uniqueArray));
+        }
+    }
+}
