347df691b94c33e9848d0c75fc63e334b5479fee
PGrytsiuk <pgrytsiuk1992@gmail.com>
2025-03-08 00:07:32 +0200

    Added method to identify if number is Armstrong

---
+++ CSharpPractice/main/math_operation/ArmstrongNumber.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpPractice.main.math_operation
+{
+    public class ArmstrongNumber
+    {
+        public static void CheckArmStrongNumber(int n)
+        {
+            int sum = 0;
+            int temp = n;
+            while (n > 0)
+            {
+                int reminder = n % 10;
+                sum += (int)Math.Pow(reminder, 3);
+                n /= 10;
+            }
+            if (temp == sum)
+            {
+                Console.WriteLine(temp + " is an Armstrong number");
+            }
+            else
+            {
+                Console.WriteLine(temp + " is not an Armstrong number");
+            }
+        }
+    }
+}
