cbcf3630d78bf1eea92d3b4e93e35dc8a41df181
PGrytsiuk <pgrytsiuk1992@gmail.com>
2025-03-15 22:30:52 +0200

    Added Factorial recursion method

---
+++ CSharpPractice/main/math_operation/FactorialRecursion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.main.math_operation
+{
+    internal class FactorialRecursion
+    {
+    }
+}
