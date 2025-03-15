8ad44ec69de6da2367eefa232397bd37c27bb9af
Pavlo <33345335+PGrytsiuk@users.noreply.github.com>
2025-03-15 22:59:03 +0200

    Develop (#29)
    * addedNewTasks
    
    * refactoring
    
    * refactoringV1.0
    
    * CountDuplicatedIntegers
    
    * addedFindedMissingNumber
    
    * AddedNewTasks
    
    * AddedRemoveDuplicatesMEthods
    
    * Adding method for showing second Largest element in the array (#22)
    
    * Added Selection Sort method (#24)
    
    * Armstrong number (#26)
    
    * Develop (#25)
    
    * addedNewTasks
    
    * refactoring
    
    * refactoringV1.0
    
    * CountDuplicatedIntegers
    
    * addedFindedMissingNumber
    
    * AddedNewTasks
    
    * AddedRemoveDuplicatesMEthods
    
    * Adding method for showing second Largest element in the array (#22)
    
    * Added Selection Sort method (#24)
    
    * Added method to identify if number is Armstrong
    
    * Convert to binary (#28)
    
    * Added ConvertToBinary method
    
    * Added Factorial recursion method
    
    * Added verification method if number is even

---
+++ CSharpPractice/main/math_operation/FactorialRecursion.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharpPractice.main.math_operation
+{
+    public class FactorialRecursion
+    {
+        public static int FactorialRecursionMethod(int factorial)
+        {
+            if (factorial == 0)
+            {
+                return 1;
+            }
+            return factorial * FactorialRecursionMethod(factorial - 1);
+        }
+    }
+}
---
+++ CSharpPractice/main/math_operation/FindEvenNumber.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpPractice.main.math_operation
+{
+    public class FindEvenNumber
+    {
+        public static void FindEvenNumberMethod(int[] numbers)
+        {
+            var isEven = numbers.Where(num => num % 2 == 0).ToList();
+            Console.WriteLine("Even numbers are: " + String.Join(", ", isEven));
+        }
+    }
+}
