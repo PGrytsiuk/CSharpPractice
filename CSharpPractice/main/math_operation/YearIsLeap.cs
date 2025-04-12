9dc07af98750170c692dea5cbba41a0c129b9760
Pavlo <33345335+PGrytsiuk@users.noreply.github.com>
2025-04-12 17:06:35 +0300

    Develop (#32)
    * addedNewTasks
    
    * refactoring
    
    * refactoringV1.0
    
    * CountDuplicatedIntegers
    
    * addedFindedMissingNumber
    
    * AddedNewTasks
    
    * AddedRemoveDuplicatesMEthods
    
    * Adding method for showing second Largest element in the array (#22)
    
    * Added Selection Sort method (#24)
    
    * Armstrong number (#26)
    
    * Develop (#25)
    
    * addedNewTasks
    
    * refactoring
    
    * refactoringV1.0
    
    * CountDuplicatedIntegers
    
    * addedFindedMissingNumber
    
    * AddedNewTasks
    
    * AddedRemoveDuplicatesMEthods
    
    * Adding method for showing second Largest element in the array (#22)
    
    * Added Selection Sort method (#24)
    
    * Added method to identify if number is Armstrong
    
    * Convert to binary (#28)
    
    * Added ConvertToBinary method
    
    * Added Factorial recursion method
    
    * Added verification method if number is even
    
    * Find GCD of two numbers
    
    * Added method if year is a leap (#31)

---
+++ CSharpPractice/main/math_operation/YearIsLeap.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpPractice.main.math_operation
+{
+    public class YearIsLeap
+    {
+
+        public static void YearIsLeapMethod(int year)
+        {
+            bool isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+            Console.WriteLine(isLeap ? $"{year} is a leap year" : $"{year} is not a leap year");
+        }
+    }
+}
