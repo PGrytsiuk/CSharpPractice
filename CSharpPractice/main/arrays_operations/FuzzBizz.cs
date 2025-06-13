8cfaebdc9aa6845d3761fdd4cd8ee28b38b3a7b0
PGrytsiuk <pgrytsiuk1992@gmail.com>
2025-06-14 02:29:19 +0300

    Added new exerceises like Stack and FuzzBizz

---
+++ CSharpPractice/main/arrays_operations/FuzzBizz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.main.arrays_operations
+{
+    public class FuzzBizz
+    {
+        public static void FuzzBizzOperation(int[] arr )
+        {
+            foreach(int num in arr)
+            {
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("FuzzBizz");
+                }
+                else if (num % 3 == 0 )
+                {
+                    Console.WriteLine("Fuzz");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Bizz");
+                }
+                else if(num % 3 != 0 && num % 5 != 0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
